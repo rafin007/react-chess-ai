e6147c48651bb634eb5987da53f34f93e97625ad
rafin007 <arefinm@lakeheadu.ca>
2022-03-18 03:24:37 -0400

    Player vs Random implemented

--- src/components/Chess/Chess.js
+++ src/components/Chess/Chess.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Chessboard } from "react-chessboard";
+import PlayVsRandom from "../PlayerVsRandom/PlayerVsRandom";
 import classes from "./Chess.module.scss";
 
 const Chess = () => {
+  const [boardSize, setBoardSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const getBoard = document.querySelector(`.${classes.chessboard}`);
+      setBoardSize(getBoard.offsetWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      // window.removeEventListener("resize");
+    };
+  }, [boardSize]);
+
   return (
     <div className={classes.chess}>
       <div className={classes.chessboard}>
-        <Chessboard id="board" />
+        {/* <Chessboard id="board" /> */}
+        <PlayVsRandom boardWidth={boardSize} />
       </div>
     </div>
   );
